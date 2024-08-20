f32e96190937884f9f4cb2bc0414d14be0648006
ortem917 <sarg.22@uni-dubna.ru>
2024-08-20 13:01:00 +0500

    mod helper and add app in __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,35 @@
 import os
+import json
 
-from flask import Flask
+from flask import (
+    Flask, jsonify, request
+)
+
+import helper
+
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+    hlp = helper.Helper()
+    @app.route('/ping')
+    def ping():
+        if not hlp.model_is_ready:
+            return jsonify(status="nor ready")
+        return jsonify(status="ready")
+    
+    @app.route('/query', methods=['POST'])
+    def query():
+        if not hlp.model_is_ready or not hlp.index_is_ready:
+            return json.dumps({"status": "FAISS is not initialized!"})
+        suggestions, lang_check = hlp.query_handler(request)
+
+        return jsonify(suggestions=suggestions, lang_check=lang_check)
+    
+    @app.route('/update_index', methods=['POST'])
+    def update_index():
+        index_size = hlp.index_handler(request)
+
+        return jsonify(index_size=index_size)
+    
+    hlp.prepare_model()
+        
--- app/helper.py
+++ app/helper.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 import torch
 import nltk
+from langdetect import detect
 
 
 import knrm
@@ -21,7 +22,7 @@ class Helper:
         self.mlp_path = os.environ['MLP_PATH']
         torch.set_grad_enabled(False)
 
-    def prepare_models(self):
+    def prepare_model(self):
         self.model = knrm.KNRM(self.emb_path_knrm, self.mlp_path)
         with open(self.vocab_path, 'r') as f:
             self.vocab = json.load(f)
@@ -77,3 +78,39 @@ class Helper:
                        query: str, ret_k: int = 10,
                        ann_k: int = 100) -> List[Tuple[str, str]]:
         q_tokens = self._simple_preproc(query)
+        vector = [self.vocab.get(tok, self.vocab['OOV']) for tok in q_tokens]
+        emb_layer = self.model.embedding.state_dict()['weight']
+        q_emb = emb_layer[vector].mean(dim=0).reshape(1, -1)
+        q_emb = np.array(q_emb).astype(np.float32)
+        _, I = self.index.search(q_emb, k=ann_k)
+        cands = [(str(i), self.documents[str(i)]) for i in I[0] if i != -1]
+        inputs = dict()
+        inputs['query'] = self._text_to_token_ids([query] * len(cands))
+        inputs['document'] = self._text_to_token_ids([cnd[1] for cnd in cands])
+        scores = self.model(inputs)
+        res_ids = scores.reshape(-1).argsort(descending=True)
+        res_ids = res_ids[:ret_k]
+        res = [cands[i] for i in res_ids.tolist()]
+        return res
+    
+    def query_handler(self, inp: Dict[str, str]):
+        input_json = json.loads(inp.json)
+        queries = input_json['queries']
+        lang_check = []
+        suggestions = []
+        for q in queries:
+            is_en = detect(q) == 'en'
+            lang_check.append(is_en)
+            if not is_en:
+                suggestions.append(None)
+                continue
+            suggestion = self.get_suggestion(q)
+            suggestions.append(suggestion)
+        return suggestions, lang_check
+    
+    def index_handler(self, inp: Dict):
+        input_json = json.loads(inp.json)
+        documents = input_json['documents']
+        index_size = self.prepare_index(documents)
+        return index_size
+    
